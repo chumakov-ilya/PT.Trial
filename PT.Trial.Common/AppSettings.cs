04e2656858514342e00569e2c1d9b4a740ec3402
Chumakov Ilya <aidforsoft@mail.ru>
2016-12-22 18:40:21 +0300

    minor fix.

--- PT.Trial.Common/AppSettings.cs
+++ PT.Trial.Common/AppSettings.cs
@@ -3,8 +3,9 @@
     public class AppSettings
     {
         public string BusConnectionString { get; set; } = "host=localhost;persistentMessages=false";
+
         public string WebConnectionString { get; set; } = "http://localhost:42424";
 
-        public int MaxNumberCount { get; set; } = 100;
+        public int MaxNumberCount { get; set; } = 5000;
     }
 }
